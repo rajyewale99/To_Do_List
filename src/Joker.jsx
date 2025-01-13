5b2b04b53710b857e86bc4685ed665ad405df965
Pruthviraj Yewale <rajyewale99@gmail.com>
2025-01-14 00:25:21 +0530

    Add Joker.jsx

---
+++ src/Joker.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+export default function Joker(){
+   
+    const URL = "https://official-joke-api.appspot.com/random_joke";
+    
+    const getNewJoke = async () => {
+       let response = await fetch (URL);
+       let jsonResponse = await response.json();
+    //    console.log(jsonResponse);
+    //    return jsonResponse;
+       setJoke({setup: jsonResponse.setup, punchline:jsonResponse.punchline});
+
+    };
+    useEffect(() => {async function getFirstJoke(){
+        let response = await fetch (URL);
+        let jsonResponse = await response.json();
+        console.log(jsonResponse);
+        setJoke({setup: jsonResponse.setup, punchline:jsonResponse.punchline});
+    }
+    
+}, []);
+   
+    let [joke, setJoke] = useState(getNewJoke);
+    return(
+        <div>
+            <h3>Joker</h3>
+            <h2>{joke.setup}</h2>
+            <h2>{joke.punchline}</h2>
+            <button onClick={getNewJoke}> New Joke</button>
+        </div>
+    )
+}
